c4568a9973362e5cea635595323bb39de90a360c
AkashMAbrrar <madborakash48@gmail.com>
2025-02-20 16:58:42 +0600

    this is the first commit for thproject set up complete with react tailwind daisy firebase etc

---
+++ src/layouts/Root.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Outlet } from "react-router";
+import Navbar from "../components/Navbar";
+
+const Root = () => {
+  return (
+    <div>
+      <header>
+        <Navbar></Navbar>
+      </header>
+      <Outlet></Outlet>
+    </div>
+  );
+};
+
+export default Root;
